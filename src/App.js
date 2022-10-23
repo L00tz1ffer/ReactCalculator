4f66127ebf89f991769de9f992724ab558789164
L00tz1ffer <88904868+L00tz1ffer@users.noreply.github.com>
2022-10-23 15:33:21 +0200

    Autogenerated Commit from  -> Zeit: 23.10.2022 15:33:21

--- src/App.js
+++ src/App.js
@@ -10,7 +10,15 @@ const App = () => {
         setResult("");
     }
     const backspace = () =>{
-
+        setResult(result.slice(0, result.length -1));
+    }
+    const calculate = () =>{
+        try {
+            setResult(eval(result).toString()) 
+        }catch (err) {
+            setResult ("Error")
+        }
+        
     }
 
 
@@ -22,24 +30,24 @@ const App = () => {
                     <input type="text" value={result}></input>
                 </form>
                 <div className='keypad'>
-                    <button className='keypad_button' onClick={clear} id="clear">Clear</button>
-                    <button className='keypad_button' onClick={backspace} id="backspace">C</button>
-                    <button className='keypad_button' name="/" onClick={handleClick}>&divide;</button>
+                    <button className='keypad_button highlight' onClick={clear} id="clear">Clear</button>
+                    <button className='keypad_button highlight' onClick={backspace} id="backspace">C</button>
+                    <button className='keypad_button highlight' name="/" onClick={handleClick}>&divide;</button>
                     <button className='keypad_button' name="7" onClick={handleClick}>7</button>
                     <button className='keypad_button' name="6" onClick={handleClick}>8</button>
                     <button className='keypad_button' name="9" onClick={handleClick}>9</button>
-                    <button className='keypad_button' name="*" onClick={handleClick}>&times;</button>
+                    <button className='keypad_button highlight' name="*" onClick={handleClick}>&times;</button>
                     <button className='keypad_button' name="4" onClick={handleClick}>4</button>
                     <button className='keypad_button' name="5" onClick={handleClick}>5</button>
                     <button className='keypad_button' name="6" onClick={handleClick}>6</button>
-                    <button className='keypad_button' name="-" onClick={handleClick}>&ndash;</button>
+                    <button className='keypad_button highlight' name="-" onClick={handleClick}>&ndash;</button>
                     <button className='keypad_button' name="1" onClick={handleClick}>1</button>
                     <button className='keypad_button' name="2" onClick={handleClick}>2</button>
                     <button className='keypad_button' name="3" onClick={handleClick}>3</button>
-                    <button className='keypad_button' name="+" onClick={handleClick}>+</button>
+                    <button className='keypad_button highlight' name="+" onClick={handleClick}>+</button>
                     <button className='keypad_button' name="0" onClick={handleClick}>0</button>
                     <button className='keypad_button' name="." onClick={handleClick}>.</button>
-                    <button className='keypad_button' onClick={handleClick} id="result">=</button>
+                    <button className='keypad_button highlight' onClick={calculate} id="result">=</button>
                 </div>
             </div>
             <h1>{result}</h1>
