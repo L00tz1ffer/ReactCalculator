273d7cee8705fb26a6440be375e4ff84219b4c2f
L00tz1ffer <88904868+L00tz1ffer@users.noreply.github.com>
2022-10-23 14:30:35 +0200

    Autogenerated Commit from  -> Zeit: 23.10.2022 14:30:35

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 const App = () => {
     const [result, setResult] = useState("test");
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name));
+    }
+    const clear = () =>{
+        setResult("");
+    }
+    const backspace = () =>{
+
+    }
+
+
+
     return (
         <>
             <div className='container'>
@@ -10,24 +22,24 @@ const App = () => {
                     <input type="text" value={result}></input>
                 </form>
                 <div className='keypad'>
-                    <button className='keypad_button'>Clear</button>
-                    <button className='keypad_button'>C</button>
-                    <button className='keypad_button'>&divide;</button>
-                    <button className='keypad_button'>7</button>
-                    <button className='keypad_button'>8</button>
-                    <button className='keypad_button'>9</button>
-                    <button className='keypad_button'>&times;</button>
-                    <button className='keypad_button'>4</button>
-                    <button className='keypad_button'>5</button>
-                    <button className='keypad_button'>6</button>
-                    <button className='keypad_button'>&ndash;</button>
-                    <button className='keypad_button'>1</button>
-                    <button className='keypad_button'>2</button>
-                    <button className='keypad_button'>3</button>
-                    <button className='keypad_button'>+</button>
-                    <button className='keypad_button'>0</button>
-                    <button className='keypad_button'>.</button>
-                    <button className='keypad_button'>=</button>
+                    <button className='keypad_button' onClick={clear} id="clear">Clear</button>
+                    <button className='keypad_button' onClick={backspace} id="backspace">C</button>
+                    <button className='keypad_button' name="/" onClick={handleClick}>&divide;</button>
+                    <button className='keypad_button' name="7" onClick={handleClick}>7</button>
+                    <button className='keypad_button' name="6" onClick={handleClick}>8</button>
+                    <button className='keypad_button' name="9" onClick={handleClick}>9</button>
+                    <button className='keypad_button' name="*" onClick={handleClick}>&times;</button>
+                    <button className='keypad_button' name="4" onClick={handleClick}>4</button>
+                    <button className='keypad_button' name="5" onClick={handleClick}>5</button>
+                    <button className='keypad_button' name="6" onClick={handleClick}>6</button>
+                    <button className='keypad_button' name="-" onClick={handleClick}>&ndash;</button>
+                    <button className='keypad_button' name="1" onClick={handleClick}>1</button>
+                    <button className='keypad_button' name="2" onClick={handleClick}>2</button>
+                    <button className='keypad_button' name="3" onClick={handleClick}>3</button>
+                    <button className='keypad_button' name="+" onClick={handleClick}>+</button>
+                    <button className='keypad_button' name="0" onClick={handleClick}>0</button>
+                    <button className='keypad_button' name="." onClick={handleClick}>.</button>
+                    <button className='keypad_button' onClick={handleClick}>=</button>
                 </div>
             </div>
             <h1>{result}</h1>
