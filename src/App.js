dadc86cab37690a03ab7f99206e6fd3fdc7952a7
L00tz1ffer <88904868+L00tz1ffer@users.noreply.github.com>
2022-10-23 14:18:26 +0200

    Autogenerated Commit from  -> Zeit: 23.10.2022 14:18:26

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ const App = () => {
                 <form>
                     <input type="text" value={result}></input>
                 </form>
-                <div className='input'>
-                    <button>Clear</button>
-                    <button>C</button>
-                    <button>&divide;</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button>&times;</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>&ndash;</button>
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>+</button>
-                    <button>0</button>
-                    <button>.</button>
-                    <button>=</button>
+                <div className='keypad'>
+                    <button className='keypad_button'>Clear</button>
+                    <button className='keypad_button'>C</button>
+                    <button className='keypad_button'>&divide;</button>
+                    <button className='keypad_button'>7</button>
+                    <button className='keypad_button'>8</button>
+                    <button className='keypad_button'>9</button>
+                    <button className='keypad_button'>&times;</button>
+                    <button className='keypad_button'>4</button>
+                    <button className='keypad_button'>5</button>
+                    <button className='keypad_button'>6</button>
+                    <button className='keypad_button'>&ndash;</button>
+                    <button className='keypad_button'>1</button>
+                    <button className='keypad_button'>2</button>
+                    <button className='keypad_button'>3</button>
+                    <button className='keypad_button'>+</button>
+                    <button className='keypad_button'>0</button>
+                    <button className='keypad_button'>.</button>
+                    <button className='keypad_button'>=</button>
                 </div>
             </div>
             <h1>{result}</h1>
